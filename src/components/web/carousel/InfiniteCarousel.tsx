49a30d4deeae9ade0162e5ebc5cf63f00508a5a3
dldnsgkr <leednsgkr@naver.com>
2024-07-21 23:17:59 +0900

    refactor: install swiper. plus home carousel image. add swiper in InfiniteCarousel.

--- src/components/web/carousel/InfiniteCarousel.tsx
+++ src/components/web/carousel/InfiniteCarousel.tsx
@@ -1,33 +1,44 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import TestImage from '../../../assets/test/home__carousel_test-image.avif';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+import '@style/web/components/carousel/infiniteCarousel.scss';
 
 const InfiniteCarousel = () => {
-  const settings = {
-    dots: false,
-    infinite: true, // 무한 스크롤
-    speed: 500, // 애니메이션 속도
-    slidesToShow: 1, // 한 번에 표시할 슬라이드 수
-    slidesToScroll: 1, // 한 번에 스크롤할 슬라이드 수
-    autoplay: true, // 자동 재생
-    autoplaySpeed: 2000, // 자동 재생 속도 (ms)
-    cssEase: 'linear', // 부드러운 스크롤
-  };
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>Slide 1</h3>
-      </div>
-      <div>
-        <h3>Slide 2</h3>
-      </div>
-      <div>
-        <h3>Slide 3</h3>
-      </div>
-      <div>
-        <h3>Slide 4</h3>
-      </div>
-    </Slider>
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="blog-wrap">
+            <img src={TestImage} alt="testimage" />
+            <div className="blog-wrap__text-container">
+              <p>제목</p>
+              <span>글쓴이?</span>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
--- src/pages/web/Home.tsx
+++ src/pages/web/Home.tsx
@@ -1,7 +1,12 @@
+import InfiniteCarousel from '@components/web/carousel/InfiniteCarousel';
 import '@style/web/pages/web/home.scss';
 
 const Home = () => {
-  return <main></main>;
+  return (
+    <main>
+      <InfiniteCarousel />
+    </main>
+  );
 };
 
 export default Home;
